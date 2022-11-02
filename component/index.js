f133f1ab689ce81a0231604519a7041ebc937600
zeynepbozkır <bozkrzeynep@gmail.com>
2022-11-02 23:15:01 +0300

    feth try

---
+++ component/index.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Movies = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "769a84efb3msh8f59bd4c654c15fp105b18jsnaf8ceb6a4a81",
+      "X-RapidAPI-Host": "mdblist.p.rapidapi.com",
+    },
+  };
+
+  fetch("https://mdblist.p.rapidapi.com/?s=jaws", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
+  return <div></div>;
+};
+
+export default Movies;
