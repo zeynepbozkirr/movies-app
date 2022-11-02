39d2fd4f80a63e04e366e09c0594b95009fe388b
zeynepbozkır <bozkrzeynep@gmail.com>
2022-11-02 23:38:10 +0300

    add

--- component/index.js
+++ component/index.js
@@ -6,11 +6,11 @@ const Movies = () => {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "769a84efb3msh8f59bd4c654c15fp105b18jsnaf8ceb6a4a81",
-      "X-RapidAPI-Host": "latest-movies.p.rapidapi.com",
+      "X-RapidAPI-Host": "movies-news1.p.rapidapi.com",
     },
   };
 
-  fetch("https://latest-movies.p.rapidapi.com/movies", options)
+  fetch("https://movies-news1.p.rapidapi.com/movies_news/recent", options)
     .then((response) => response.json())
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
