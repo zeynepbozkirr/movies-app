5375479319130ca2eb1a60f8de7c97178368a3fb
zeynepbozkır <bozkrzeynep@gmail.com>
2022-11-02 23:19:28 +0300

    fetch a film in actor

--- component/index.js
+++ component/index.js
@@ -6,11 +6,14 @@ const Movies = () => {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "769a84efb3msh8f59bd4c654c15fp105b18jsnaf8ceb6a4a81",
-      "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+      "X-RapidAPI-Host": "movie-details1.p.rapidapi.com",
     },
   };
 
-  fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", options)
+  fetch(
+    "https://movie-details1.p.rapidapi.com/imdb_api/movie?id=tt1375666",
+    options
+  )
     .then((response) => response.json())
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
