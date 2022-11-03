ee51eea477c1aa552345525450fa2c199dea38b2
zeynepbozkır <bozkrzeynep@gmail.com>
2022-11-03 14:17:44 +0300

    add fetch

--- component/index.js
+++ component/index.js
@@ -1,21 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const Movies = () => {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "769a84efb3msh8f59bd4c654c15fp105b18jsnaf8ceb6a4a81",
-      "X-RapidAPI-Host": "movies-news1.p.rapidapi.com",
-    },
-  };
+const Movies = ({ movies }) => {
+  console.log(movies, "mm");
 
-  fetch("https://movies-news1.p.rapidapi.com/movies_news/recent", options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-
-  return <div></div>;
+  return (
+    <div>
+      <ul>{movies}</ul>
+      ll
+    </div>
+  );
 };
 
 export default Movies;
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,35 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Movies from "../component";
 
-export default function Home() {
+export default function Home({ movies }) {
+  console.log(movies, "sss");
   return (
     <div className={styles.container}>
-      <Movies />
+      {/*<Movies />*/}
+      {movies.map((data) => (
+        <img src={data.cover} alt={"photo"} />
+      ))}
       ss
     </div>
   );
+}
+export async function getStaticProps() {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "769a84efb3msh8f59bd4c654c15fp105b18jsnaf8ceb6a4a81",
+      "X-RapidAPI-Host": "movie-quote.p.rapidapi.com",
+    },
+  };
+
+  const movies = await fetch(
+    "https://movie-quote.p.rapidapi.com/movie/so%20far%20so%20good",
+    options
+  ).then((response) => response.json());
+
+  return {
+    props: {
+      movies,
+    },
+  };
 }
