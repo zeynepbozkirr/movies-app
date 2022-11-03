44e8b183713ce96e8fb120e83865b532868791dc
zeynepbozkır <bozkrzeynep@gmail.com>
2022-11-03 15:36:21 +0300

    add

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,34 @@ import Movies from "../component";
 
 export default function Home({ movies }) {
   console.log(movies, "sss");
+
   return (
     <div className={styles.container}>
       {/*<Movies />*/}
-      {movies.map((data) => (
-        <img src={data.cover} alt={"photo"} />
+      {movies.results.map((a) => (
+        <img src={a.image} />
       ))}
       ss
     </div>
   );
 }
 export async function getStaticProps() {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "769a84efb3msh8f59bd4c654c15fp105b18jsnaf8ceb6a4a81",
-      "X-RapidAPI-Host": "movie-quote.p.rapidapi.com",
-    },
-  };
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Key": "769a84efb3msh8f59bd4c654c15fp105b18jsnaf8ceb6a4a81",
+  //     "X-RapidAPI-Host": "movie-quote.p.rapidapi.com",
+  //   },
+  // };
+  //
+  // const movies = await fetch(
+  //   "https://movie-quote.p.rapidapi.com/movie/so%20far%20so%20good",
+  //   options
+  // ).then((response) => response.json());
 
-  const movies = await fetch(
-    "https://movie-quote.p.rapidapi.com/movie/so%20far%20so%20good",
-    options
-  ).then((response) => response.json());
+  const movies = await fetch("https://rickandmortyapi.com/api/character").then(
+    (response) => response.json()
+  );
 
   return {
     props: {
